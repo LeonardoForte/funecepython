c598661ff4cc394fca2763e6468545eee1e0c2a5
LeonardoForte <82840190+LeonardoForte@users.noreply.github.com>
2021-10-26 22:48:17 -0300

    Add files via upload
    Tentativa de resolução do desafio.

---
+++ desafios/main.py
@@ -0,0 +1,12 @@
+# INSERINDO OS DADOS:
+
+nome= input("Digite seu nome: ")
+idade= input("Digite sua idade: ")
+sexo= input("Digite seu gênero: ")
+cidade= input("Digite a sua cidade: ")
+estado= input("Digite seu Estado: ")
+
+print(nome)
+print(idade)
+print(sexo)
+print(cidade)
---
+++ desafios/pessoa.py
@@ -0,0 +1,8 @@
+class Pessoa:
+    def __init__(self, nome, idade, sexo, cidade, estado):
+        self.nome = nome
+        self.idade = idade
+        self.sexo = sexo
+        self.cidade = cidade
+        self.estado =estado
+        
