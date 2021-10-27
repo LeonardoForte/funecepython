0b295408f12633581cc4c9ff7885e923ea4b043a
LeonardoForte <82840190+LeonardoForte@users.noreply.github.com>
2021-10-27 14:53:49 -0300

    Update pessoa.py

--- desafios/pessoa.py
+++ desafios/pessoa.py
@@ -4,5 +4,18 @@ class Pessoa:
         self.idade = idade
         self.sexo = sexo
         self.cidade = cidade
-        self.estado =estado
-        
+        self.estado = estado
+    
+    def mostrarinfos(self):
+        print(self.nome, self.idade, self.sexo, self.cidade, self.estado)
+
+    
+
+
+pessoa_nome = input("Digite o seu nome: ")
+pessoa_idade = input("Digite sua idade: ")
+pessoa_sexo = input("Digite seu sexo: ")
+pessoa_cidade = input("Digite sua cidade: ")
+pessoa_estado = input("Digite seu estado: ")
+
+P1 = Pessoa(pessoa_nome, pessoa_idade, pessoa_sexo, pessoa_cidade, pessoa_estado)
